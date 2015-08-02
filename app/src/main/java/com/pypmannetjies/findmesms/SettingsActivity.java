d9678d70977e694186ceb9c4aa0a8f7e07f057b6
Christien Kroeze <pypmannetjies@gmail.com>
2015-08-02 10:41:12 +0200

    Removed the menu

--- app/src/main/java/com/pypmannetjies/findmesms/SettingsActivity.java
+++ app/src/main/java/com/pypmannetjies/findmesms/SettingsActivity.java
@@ -37,25 +37,6 @@ public class SettingsActivity extends Activity {
         passphraseView.setText(settingsStorage.getPassphraseFromStorage());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     public void storeValues(View view) {
         String email = emailView.getText().toString();
         String message = passphraseView.getText().toString();
